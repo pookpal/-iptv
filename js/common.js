f2b4d05fd5805fa7f05cba7b1619555f23f17417
pookpal <pookpal@163.com>
2015-06-19 11:48:28 +0800

    优化
    类名语义化修改

--- js/common.js
+++ js/common.js
@@ -1,15 +1,12 @@
 /**
  * Created by lijie on 15/6/10.
  * 待解决问题
- * 1、昵称和聊天信息框获取焦点时自动清空
  */
 
-
 $(function () {
-
-    var channleItemWidth = $('ul.channleItem').width();
     //频道导航切换效果
     $('ul.tabChannle li').click(function () {
+        var channleItemWidth = $('ul.channleItem').width();
         var chatBoxDisplay = getStyle('display',$('.chatBox .chatContent')[0]);
         var index = $('ul.tabChannle li').index(this);
         if(chatBoxDisplay =="block"){
@@ -19,15 +16,7 @@ $(function () {
         $(this).siblings('li').removeClass('current');
 
         slide(index, channleItemWidth);
-
     });
-    //滑动函数
-    function slide(index, channleItemWidth){
-        console.log(channleItemWidth);
-        $('div.channleMenu').animate(
-            {marginLeft:- index * channleItemWidth+"px"},300
-        );
-    }
 
     //频道菜单点击切换效果
     $('ul.channleItem li a').click(function () {
@@ -37,7 +26,8 @@ $(function () {
 
     //点击meta打开聊天框
     $('.chatBox .meta').not('.lamp').click(function(){
-        //聊天窗口
+
+        //聊天窗口打开
         $('.chatBox .chatContent').slideToggle('slow');
 
         //chatBox 滚动条
@@ -50,20 +40,26 @@ $(function () {
 
     //点击lamp切换背景灯
     $('.lamp').click(function(){
-        var wrapDisplay = getStyle('display',$('.wrap')[0]);
+        var wrapDisplay = getStyle('display',$('.mask')[0]);
 
         if(wrapDisplay == "none"){
-            $('.wrap').css({"display":"block"});
+            $('.mask').css({"display":"block"});
             $('.leftSide').css({"zIndex":"4"});
             $('.chatBox .lamp').css({"zIndex":"4"});
 
         }else{
-            $('.wrap').css({"display":"none"});
+            $('.mask').css({"display":"none"});
             $('.leftSide').css({"zIndex":"0"});
             $('.chatBox .lamp').css({"zIndex":"0"});
         }
 
     });
+
+    //滑动函数
+    function slide(index, channleItemWidth){
+        $('div.channleMenu').animate(
+            {marginLeft:- index * channleItemWidth+"px"},300);
+    }
 
     //自定义函数获取对象的样式
     function getStyle(attr,obj){
